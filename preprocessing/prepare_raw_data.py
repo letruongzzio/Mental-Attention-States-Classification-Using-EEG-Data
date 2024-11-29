29b35e6ba27a4fcbe5fb87dd8754abd0c5771ba0
thangquang09 <thangquangly0909@gmail.com>
2024-11-29 22:16:13 +0700

    merge

--- preprocessing/prepare_raw_data.py
+++ preprocessing/prepare_raw_data.py
@@ -21,14 +21,17 @@ def download_data() -> list:
         print(path)
     except ConnectionError:
         path = "~/.cache/kagglehub/datasets/inancigdem/eeg-data-for-mental-attention-state-detection/versions/1"
-        path = os.path.expanduser(path) # for all user, all OS
+        path = os.path.expanduser(path)  # for all user, all OS
     data_path = os.path.join(path, "EEG Data")
     data_files: list[str] = [
-        os.path.join(data_path, file) for file in os.listdir(data_path) if "record" in file
+        os.path.join(data_path, file)
+        for file in os.listdir(data_path)
+        if "record" in file
     ]
     data_files.sort(key=lambda x: int(x.split("eeg_record")[1].split(".mat")[0]))
 
     return data_files
+
 
 def prepare_matlab_file(matlab_file_path: str) -> pd.DataFrame:
     """
@@ -41,12 +44,17 @@ def prepare_matlab_file(matlab_file_path: str) -> pd.DataFrame:
     df = df.reset_index()
     df = df.rename(columns={"index": "t"})
 
-    if df['ED_INTERPOLATED'].sum() > 0:
+    if df["ED_INTERPOLATED"].sum() > 0:
         warnings.warn("Have interpolated values", UserWarning)
-    
+
     return df
 
-def extract_data(matlab_df: pd.DataFrame, take_useful_channels: bool = False, skip_first_5s: bool = False) -> pd.DataFrame:
+
+def extract_data(
+    matlab_df: pd.DataFrame,
+    take_useful_channels: bool = False,
+    skip_first_5s: bool = False,
+) -> pd.DataFrame:
     """
     Return filtered_df with columns are: t, channels, state
     """
@@ -69,11 +77,11 @@ def extract_data(matlab_df: pd.DataFrame, take_useful_channels: bool = False, sk
         else:
             return "unfocused"
 
-    matlab_df = matlab_df.copy() # To fix warning
+    matlab_df = matlab_df.copy()  # To fix warning
     matlab_df["state"] = matlab_df["t"].apply(get_state)
 
     if skip_first_5s:
-        matlab_df = matlab_df.iloc[5*FS:, :]
+        matlab_df = matlab_df.iloc[5 * FS :, :]
 
     # Preprocess the data
     filtered_df = preprocess_eeg_dataframe(matlab_df, channel_columns[1:])
