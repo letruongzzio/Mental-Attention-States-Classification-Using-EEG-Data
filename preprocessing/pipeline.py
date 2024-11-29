b94e23642e9608bc753971df7c814f1c249de98c
thangquang09 <thangquangly0909@gmail.com>
2024-11-30 00:40:58 +0700

    Upload raw data

--- preprocessing/pipeline.py
+++ preprocessing/pipeline.py
@@ -6,31 +6,46 @@ from prepare_raw_data import download_data, prepare_matlab_file, extract_data
 TRAIN_PATH = "./data/df_train.csv"
 TEST_PATH = "./data/df_test.csv"
 
-if __name__ == "__main__":
-    print("Downloading Data...")
+def load_raw_data(OUTPUT_FOLDER='./data/raw'):
     mat_paths = download_data()
-    train_df: pd.DataFrame = None
-    test_df: pd.DataFrame = None
+    subject = 1
+    for i in range(1, 34, 7):
+        print(f"Loading subject {subject} raw data...")
+        df_subject = None
+        for file_num in range(i, min(i + 7, 35)):
+            matlab_df = prepare_matlab_file(mat_paths[file_num-1])
+            df_subject = matlab_df if df_subject is None else pd.concat([df_subject, matlab_df])
+        df_subject.reset_index(drop=True, inplace=True)
+        output_path = os.path.join(OUTPUT_FOLDER, f"raw_{subject}.csv")
+        df_subject.to_csv(output_path, index=False)
+        subject += 1
 
-    print("Processing...")
-    for idx, path in enumerate(mat_paths):
-        matlab_df = prepare_matlab_file(path)
-        preprocessed_df = extract_data(
-            matlab_df, take_useful_channels=True, skip_first_5s=True
-        )
-        fe_df = feature_extraction(preprocessed_df, WINDOW_LENGTH, STEP_RATE)
-        if (
-            1 <= (idx + 1) % 7 <= 2
-        ):  # if file is recorded in the first and the second day -> test
-            test_df = fe_df if test_df is None else pd.concat([test_df, fe_df])
-        else:
-            train_df = fe_df if train_df is None else pd.concat([train_df, fe_df])
+if __name__ == "__main__":
+    # print("Downloading Data...")
+    # mat_paths = download_data()
+    # train_df: pd.DataFrame = None
+    # test_df: pd.DataFrame = None
 
-    print("Train DataFrame Shape:", train_df.shape)
-    print("Test DataFrame Shape:", test_df.shape)
+    # print("Processing...")
+    # for idx, path in enumerate(mat_paths):
+    #     matlab_df = prepare_matlab_file(path)
+    #     preprocessed_df = extract_data(
+    #         matlab_df, take_useful_channels=True, skip_first_5s=True
+    #     )
+    #     fe_df = feature_extraction(preprocessed_df, WINDOW_LENGTH, STEP_RATE)
+    #     if (
+    #         1 <= (idx + 1) % 7 <= 2
+    #     ):  # if file is recorded in the first and the second day -> test
+    #         test_df = fe_df if test_df is None else pd.concat([test_df, fe_df])
+    #     else:
+    #         train_df = fe_df if train_df is None else pd.concat([train_df, fe_df])
 
-    train_df.to_csv(TRAIN_PATH, index=False)
-    train_df.to_csv(TEST_PATH, index=False)
+    # print("Train DataFrame Shape:", train_df.shape)
+    # print("Test DataFrame Shape:", test_df.shape)
 
-    print("Train DataFrame is saved at", TRAIN_PATH)
-    print("Test DataFrame is saved at", TEST_PATH)
+    # train_df.to_csv(TRAIN_PATH, index=False)
+    # train_df.to_csv(TEST_PATH, index=False)
+
+    # print("Train DataFrame is saved at", TRAIN_PATH)
+    # print("Test DataFrame is saved at", TEST_PATH)
+    pass
